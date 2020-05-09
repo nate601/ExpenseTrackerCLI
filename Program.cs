cf2f19accb79775290819eabac75be124b421dd8
Nathan Button <nathanebutton@gmail.com>
2020-05-09 16:50:25 +0000

    Remove wasteful usings

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using expenseTrackerCli.Database;
-using static expenseTrackerCli.ConsoleUtilities;
 
 namespace expenseTrackerCli
 {

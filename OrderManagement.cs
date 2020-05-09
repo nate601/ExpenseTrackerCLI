a705cb50d99413099b05bcc8e1a44bd4ba075310
Nathan Button <nathanebutton@gmail.com>
2020-05-09 03:54:16 +0000

    Add scaffolding for order management

--- OrderManagement.cs
+++ OrderManagement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using expenseTrackerCli.Database;
 
 namespace expenseTrackerCli
@@ -16,8 +17,50 @@ namespace expenseTrackerCli
             ExpenseOrder[] orders = db.GetOrders();
             Console.Clear();
 
+            const int numberPerPage = 5;
+            int numberOfPages = orders.Length / numberPerPage;
+            int currentPageIndex = 0;
 
-            return retVal;
+            while (true)
+            {
+                if (currentPageIndex < 0)
+                {
+                    currentPageIndex = 0;
+                }
+                if (currentPageIndex > numberOfPages)
+                {
+                    currentPageIndex = numberOfPages;
+                }
+                Console.Clear();
+                ExpenseOrder[] currentPage = orders.Skip(currentPageIndex * numberPerPage).Take(numberPerPage).ToArray();
+                Console.WriteLine($"Displaying {currentPage.Length} of {orders.Length} orders.");
+
+                for (int i = 0; i < currentPage.Length; i++)
+                {
+                    ExpenseOrder order = currentPage[i];
+                    Console.WriteLine($"{i + 1} : Order from {order.OrderDate.ToShortDateString()}");
+                }
+                string resp = ConsoleUtilities.AskUser("(n)ext page, (p)rev page, (q)uit, or press a number to select an order.");
+                if (resp == "n")
+                {
+                    currentPageIndex++;
+                }
+                else if (resp == "p")
+                {
+                    currentPageIndex--;
+                }
+                else if (resp == "q")
+                {
+                    return null;
+                }
+                else if (int.TryParse(resp, out int selectedItemIndex)
+                         && selectedItemIndex > 0
+                         && selectedItemIndex <= currentPage.Length)
+                {
+                    return currentPage.ToArray()[selectedItemIndex - 1];
+                }
+            }
+
         }
 
 

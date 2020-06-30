9116467e914d0c60b6d2d24b0a73cee190f2ffa6
Nathan Button <nathanebutton@gmail.com>
2020-06-30 04:44:39 +0000

    Seperate prompt into new method, preparing for the seperation into different state classes

--- Program.cs
+++ Program.cs
@@ -9,54 +9,59 @@ namespace expenseTrackerCli
             while (true)
             {
                 Database.Database db = new Database.Database();
-                Console.WriteLine("NMF Record Generator");
-                Console.WriteLine("1: Add new Orderable");
-                Console.WriteLine("2: Add new Order");
-                Console.WriteLine("3: Edit orderable database");
-                Console.WriteLine("4: Edit order");
-                Console.WriteLine("5: Receipt");
-                Console.WriteLine("6: Print Reports");
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                        // Add New Orderable
-                        Creation.OrderablePrompt(db);
-                        break;
-                    case "2":
-                        // Add New Order
-                        Creation.OrderPrompt(db);
-                        break;
-                    case "3":
-                        OrderableManagement.DisplayOrderables(db);
-                        break;
-                    case "4":
-                        Database.ExpenseOrder editOrderOrder = OrderManagement.ChooseOrder(db);
-                        if (editOrderOrder != null)
-                        {
-                            editOrderOrder = OrderManagement.EditOrder(editOrderOrder);
-                            db.OverwriteOrder(editOrderOrder);
-                        }
-                        break;
-                    case "5":
-                        Database.ExpenseOrder receiptOrder = OrderManagement.ChooseOrderResolved(db);
-                        if (receiptOrder != null)
-                        {
-                            receiptOrder = OrderManagement.ResolveOrder(receiptOrder);
-                            if (receiptOrder == null)
-                            {
-                                break;
-                            }
+                NMFRecordPrompt(db);
+            }
+        }
 
-                            db.OverwriteOrder(receiptOrder);
+        private static void NMFRecordPrompt(Database.Database db)
+        {
+            Console.WriteLine("NMF Record Generator");
+            Console.WriteLine("1: Add new Orderable");
+            Console.WriteLine("2: Add new Order");
+            Console.WriteLine("3: Edit orderable database");
+            Console.WriteLine("4: Edit order");
+            Console.WriteLine("5: Receipt");
+            Console.WriteLine("6: Print Reports");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    // Add New Orderable
+                    Creation.OrderablePrompt(db);
+                    break;
+                case "2":
+                    // Add New Order
+                    Creation.OrderPrompt(db);
+                    break;
+                case "3":
+                    OrderableManagement.DisplayOrderables(db);
+                    break;
+                case "4":
+                    Database.ExpenseOrder editOrderOrder = OrderManagement.ChooseOrder(db);
+                    if (editOrderOrder != null)
+                    {
+                        editOrderOrder = OrderManagement.EditOrder(editOrderOrder);
+                        db.OverwriteOrder(editOrderOrder);
+                    }
+                    break;
+                case "5":
+                    Database.ExpenseOrder receiptOrder = OrderManagement.ChooseOrderResolved(db);
+                    if (receiptOrder != null)
+                    {
+                        receiptOrder = OrderManagement.ResolveOrder(receiptOrder);
+                        if (receiptOrder == null)
+                        {
+                            break;
                         }
-                        break;
-                    case "6":
-                        ReportGen.CreateNotReceiptReport(db, DateTime.Today);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Entry");
-                        break;
-                }
+
+                        db.OverwriteOrder(receiptOrder);
+                    }
+                    break;
+                case "6":
+                    ReportGen.CreateNotReceiptReport(db, DateTime.Today);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
             }
         }
     }
